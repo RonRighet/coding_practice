1807de2a4ac8c4c5c106e37fa06e0937c5d78f56
RonRighet <bgnameless@gmail.com>
2025-04-01 22:12:37 -0700

    rock_paper_scissors.css

---
+++ rock_paper_scissors.css
@@ -0,0 +1,17 @@
+const screens =document.querySelectorAll('.screen')
+const start_btn = document.getElementById('start-btn')
+const choose_choice_btn = document.querySelectorAll('.choose-choice-btn')
+const game_container=document.getElementById('game-container')
+
+start_btn.addEventListener('click', () => {
+    screens[0].classList.add('up')
+})
+
+choose_choice_btn.forEach(btn=>{
+    btn.addEventListener('click',()=>{
+        screens[1].classList.add('up')
+        const img = btn.querySelector('img')
+        startGame()
+        createInsect()
+    })
+})
